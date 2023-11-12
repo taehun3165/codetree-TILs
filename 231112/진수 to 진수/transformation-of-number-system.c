c3e5af77f4ff773c2347354a0bf6b81620bfbc8b
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-12 23:58:07 +0900

    Title: 진수 to 진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231112/진수 to 진수/transformation-of-number-system.c
+++ 231112/진수 to 진수/transformation-of-number-system.c
@@ -2,22 +2,25 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a,b,n,i,j=0,k=0,e=0,i1=0;
+    int a,b,n,i,j=0,k=0,e=0,i1[10000];
     scanf("%d %d %d",&a,&b,&n);
     for(i=1;n!=0;i*=a){
         j+=(n%10)*i;
         n=n/10;
     }
-    i=1;
+
+    i=0;
     while(1){
         if(j<b){
-            i1+=j*i;
+            i1[i]=j;
             break;
         }
-        i1+=j%b*i;
-        i*=10;
+        i1[i]=j%b;
+        i++;
         j=j/b;
     }
-    printf("%d\n",i1);
+    for(int x=i;x>=0;x--){
+        printf("%d",i1[x]);
+    }
     return 0;
 }
