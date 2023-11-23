69735bc17ae85c16d488d6088e07d1dd99fc1ea1
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-23 20:38:54 +0900

    Title: 움직이는 블록, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 231123/움직이는 블록/moving-block.c
+++ 231123/움직이는 블록/moving-block.c
@@ -2,29 +2,22 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int i,n,li[10000]={'\0'},min=0,max=0,t,c=0,x=0;
+    int i,n,li[10000]={'\0'},t,f=0,c=0,x=0,mean=0,hap=0;
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d",&li[i]);
+        hap+=li[i];
     }
-    while(1){
-        for(i=0;i<n-1;i++){
-            if(li[i]!=li[i+1])break;
-            else if(i==n-2){
-                x=1;
-            }
+    mean=hap/n;
+    for(i=0;i<n;i++){
+        if(li[i]>mean){
+            c+=li[i]-mean;
         }
-        if(x==1)break;
-        for(i=0;i<n;i++){
-            if(li[i]>li[max])max=i;
-            if(li[i]<li[min])min=i;
+        else if(li[i]<mean){
+            c+=mean-li[i];
         }
-        li[min]++;
-        li[max]--;
-
-
-        c++;
     }
-    printf("%d",c);
+    
+    printf("%d",c/2);
     return 0;
 }
