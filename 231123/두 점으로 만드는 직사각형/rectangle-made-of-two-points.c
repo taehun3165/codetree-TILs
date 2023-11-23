8052af0b5a3cb7bbb42d409f38514a262e9644de
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-23 15:11:30 +0900

    Title: 두 점으로 만드는 직사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231123/두 점으로 만드는 직사각형/rectangle-made-of-two-points.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x[4],y[4],i,xmax=0,xmin=100,ymax=0,ymin=100,f1,f2;
+    for(i=0;i<4;i++){
+        scanf("%d %d",&x[i],&y[i]);
+    }
+    for(i=0;i<4;i++){
+        if(x[i]<xmin)xmin=x[i];
+        if(x[i]>xmax)xmax=x[i];
+        if(y[i]<ymin)ymin=y[i];
+        if(y[i]>ymax)ymax=y[i];
+    }
+    f1=xmax-xmin;
+    f2=ymax-ymin;
+    printf("%d",f1*f2);
+    return 0;
+}
