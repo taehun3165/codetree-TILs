b329fa1ef1f9bfa7cfd95a212f65c2875826ce92
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-23 22:29:13 +0900

    Title: 수열의 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231123/수열의 순서 바꾸기/reorder-sequence.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,li[100]={'\0'},i,t,m,x=1,c=0,min=100;
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        scanf("%d",&li[i]);
+    }
+    if(n==1)printf("0");
+    if(n==2){
+        if(li[0]>li[1]){
+            t=li[0];
+            li[0]=li[1];
+            li[1]=t;
+        }
+    }
+    else{
+        while(1){
+            for(i=0;i<n-1;i++){
+                if(li[i]+1!=li[i+1])break;
+                else if (i==n-2){
+                    x=0;
+                    break;
+                }
+            }
+            if(x==0)break;
+            min=100;
+            for(i=2;i<n;i++){
+                if(1<=li[i]-li[0]&&min>li[i]-li[0]){
+                    min=li[i]-li[0];
+                    m=i-1;
+                }
+                else if(i+1==n&&min==100){
+                    m=i;
+                    break;
+                }
+            }
+            t=li[0];
+            for(i=1;i<=m;i++){
+                li[i-1]=li[i];
+            }
+            li[m]=t;
+            printf("%d %d %d %d\n",li[0],li[1],li[2],li[3]);
+            c++;
+        }
+    }
+    printf("%d",c);
+    return 0;
+}
