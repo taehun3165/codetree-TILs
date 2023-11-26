9ad4507bea036889b5411c6afe18c7193783191e
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-26 23:51:15 +0900

    Title: 순 간 이 동, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231126/순 간 이 동/teleportation.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int min=1000,i,a,b,x,y,j,c=0;
+    scanf("%d %d %d %d",&a,&b,&x,&y);
+    if(min>a-x+b-y&& a-x>0 && b-y>0)min=a-x+b-y;
+    if(min>b-x+a-y&& b-x>0 && a-y>0)min=b-x+a-y;
+    if(min>x-a+y-b&& x-a>0 && y-b>0)min=x-a+y-b;
+    if(min>x-b+y-a&& x-b>0 && x-b>0)min=x-b+y-a;
+
+    if(min>a-y+x-b&& a-y>0 && x-b>0)min=a-x+y-b;
+    if(min>b-y+x-a&& b-y>0 && x-a>0)min=b-x+y-a;
+    if(min>y-a+b-x&& y-a>0 && b-x>0)min=x-a+b-y;
+    if(min>y-b+a-x&& y-b>0 && a-x>0)min=y-a+b-x;
+    printf("%d",min);
+    return 0;
+}
