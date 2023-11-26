38872dc3ad0761e2662cca5e44f48a4715bd5fff
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-27 00:51:21 +0900

    Title: 운행 되고 있는 시간, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 231127/운행 되고 있는 시간/hours-in-service.c
+++ 231127/운행 되고 있는 시간/hours-in-service.c
@@ -2,20 +2,25 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n,a[101],b[101],i,j,max=0,amin=1000,bmax=0;
+    int n,a[101],b[101],i,j,l,hap,max=0;
     scanf("%d",&n);
     for(i=0;i<n;i++){
         scanf("%d %d",&a[i],&b[i]);
     }
     for(i=0;i<n;i++){
+        hap=0;
+        int x[1001]={0};
         for(j=0;j<n;j++){
-            amin=1000,bmax=0;
             if(i!=j){
-                if(amin>a[j])amin=a[j];
-                if(bmax<b[j])bmax=b[j];
+                for(l=a[j];l<b[j];l++){
+                    x[l]=1;
+                }
             }
-            if(bmax-amin>max)max=bmax-amin;
         }
+        for(j=0;j<1001;j++){
+            hap+=x[j];
+        }
+        if(max<hap)max=hap;
     }
     printf("%d",max);
     return 0;
