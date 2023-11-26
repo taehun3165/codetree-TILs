e7aaec06812157694182a31132b33af8cd2be2a9
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-27 00:22:03 +0900

    Title: 연속된 숫자 만들기 3, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231127/연속된 숫자 만들기 3/create-consecutive-numbers-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c=0,n,t;
+    scanf("%d %d %d",&a,&b,&c);
+    while(1){
+        if(a+1==b&&b+1==c)break;
+        else if(a-b>b-a){
+            a=c-1;
+            t=a;
+            a=b;
+            b=t;
+        }
+        else{
+            c=a+1;
+            t=b;
+            b=c;
+            c=t;
+        }
+        c++;
+    }
+    printf("%d",c);
+    return 0;
+}
