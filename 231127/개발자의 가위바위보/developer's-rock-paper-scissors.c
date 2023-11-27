9bfda0151601dc582d67146084a7281589a2e3c9
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2023-11-27 19:10:18 +0900

    Title: 개발자의 가위바위보, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231127/개발자의 가위바위보/developer's-rock-paper-scissors.c
+++ 231127/개발자의 가위바위보/developer's-rock-paper-scissors.c
@@ -2,18 +2,21 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int i,j,n,a[101],b[101],c1[2]={0},max=0;
+    int i,j,n,a[101],b[101],c1=0,c2=0,max=0;
     scanf("%d",&n);
     for(i=0;i<n;i++){
-        scanf("%d %d",&a[i],&b[j]);
+        scanf("%d %d",&a[i],&b[i]);
     }
     for(i=0;i<n;i++){
-        if(a[i]==1&&b[i]==2||a[i]==2&&b[i]==3||a[i]==3&&b[i]==1)c1[0]++;
-        if(a[i]==2&&b[i]==1||a[i]==3&&b[i]==2||a[i]==1&&b[i]==3)c1[1]++;
+        if((a[i]==1&&b[i]==2)||(a[i]==2&&b[i]==3)||(a[i]==3&&b[i]==1)){
+            c1++;
+            }
+        if((a[i]==2&&b[i]==1)||(a[i]==3&&b[i]==2)||(a[i]==1&&b[i]==3)){
+            c2++;
+            }
     }
-    for(i=0;i<2;i++){
-        if(max<c1[i])max=c1[i];
-    }
+    if(max<c1)max=c1;
+    if(max<c2)max=c2;
     printf("%d",max);
     return 0;
 }
