8d3f8740fff79afc66653edd9ff4fa381f69baec
taehun3165 <81422054+taehun3165@users.noreply.github.com>
2024-01-06 02:53:46 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/Run Length 인코딩/run-length-encoding.c
+++ 240106/Run Length 인코딩/run-length-encoding.c
@@ -2,13 +2,15 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int nc[501]={0},i,c=0,cn=1;
+    int nc[501]={0},i,c=0,cn=1,call=0;
     char w[1001]={'\0'},wh[501]={'\0'};
     scanf("%s",w);
     for(i=0;w[i]!='\0';i++){
         if(w[i]!=w[i+1]){
             nc[c]=cn;
             wh[c]=w[i];
+            if(cn>=100)call+=2;
+            else if(cn>=10)call+=1;
             cn=1;
             c++;
         }
@@ -16,7 +18,8 @@ int main() {
             cn++;
         }
     }
-    printf("%d\n",c*2);
+    call+=c*2;
+    printf("%d\n",call);
     for(i=0;i<c;i++){
         printf("%c%d",wh[i],nc[i]);
     }
